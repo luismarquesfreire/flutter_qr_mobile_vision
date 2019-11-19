651e194ca17340fab5bef368f6ad4763d30de9ab
Luis Freire <luis.freire@hypnotic.pt>
2019-11-19 22:03:15 +0000

    Update dependencies

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/luisfreire/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/luisfreire/Projects/Personal/flutter_qr_mobile_vision/example"
+export "FLUTTER_TARGET=/Users/luisfreire/Projects/Personal/flutter_qr_mobile_vision/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/luisfreire/development/flutter/bin/cache/artifacts/engine/ios"
+export "TRACK_WIDGET_CREATION=true"
